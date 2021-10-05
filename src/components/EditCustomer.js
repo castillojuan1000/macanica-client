a8478e209341d58003a6f9e820bd6a02322c60ac
Juan Castillo <42900789+castillojuan1000@users.noreply.github.com>
2021-10-05 18:51:04 -0400

    #13 created form to edit customer

--- src/components/EditCustomer.js
+++ src/components/EditCustomer.js
@@ -3,14 +3,34 @@ import Button from '@mui/material/Button';
 import TextField from '@mui/material/TextField';
 import Dialog from '@mui/material/Dialog';
 import DialogActions from '@mui/material/DialogActions';
-import DialogContent from '@mui/material/DialogContent';
-import DialogContentText from '@mui/material/DialogContentText';
-import DialogTitle from '@mui/material/DialogTitle';
 import IconButton from '@mui/material/IconButton';
 import EditIcon from '@mui/icons-material/Edit';
+import { makeStyles } from '@material-ui/core';
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: theme.spacing(2),
+'& .MuiTextField-root': {
+      margin: theme.spacing(1),
+      width: '300px',
+    },
+'& .MuiButtonBase-root': {
+      margin: theme.spacing(2),
+    },
+  },
+}));
 
 export default function EditCustomer({customer}) {
+  const {FirstName, LastName, Phone} = customer
+  const classes = useStyles();
   const [open, setOpen] = useState(false);
+  const [firstName, setFirstName] = useState(FirstName);
+  const [lastName, setLastName] = useState(LastName);
+  const [phoneNumber, setPhoneNumber] = useState(Phone)
 
   const handleClickOpen = (event) => {
     event.stopPropagation();
@@ -23,53 +43,84 @@ export default function EditCustomer({customer}) {
   };
 
   const handleEditCustomer = (event) => {
-    event.stopPropagation();
-    alert("Edit customer")
+    event.preventDefault();
+    
+    console.log({
+      "first Name": firstName, 
+      "last Name": lastName, 
+      "phone Number": phoneNumber
+    })
     setOpen(false);
+    event.stopPropagation();
   }
+
 
   return (
     <div>
       <IconButton 
-        aria-label="delete" 
+        aria-label="edit" 
         color="warning"
         onClick={handleClickOpen}
       >
         <EditIcon />
       </IconButton>
       <Dialog open={open} onClose={handleClose}>
-        <DialogTitle>Editar Cliente</DialogTitle>
-        <DialogContent>
-          <DialogContentText>
-            To subscribe to this website, please enter your email address here. We
-            will send updates occasionally.
-          </DialogContentText>
+        <form 
+          className={classes.root}
+          onSubmit={handleEditCustomer}
+        >
           <TextField
-            autoFocus
-            margin="dense"
-            id="name"
-            label="Email Address"
-            type="email"
+            autoComplete="fname"
+            name="firstName"
+            required
             fullWidth
-            variant="standard"
+            id="firstName"
+            label="First Name"
+            autoFocus
+            value={firstName}
+            onChange={e => setFirstName(e.target.value)}
           />
-        </DialogContent>
-        <DialogActions>
-          <Button 
-            onClick={handleClose}
-            variant="outlined" 
-            color="success"
-            >
-              Cancelar
-          </Button>
-          <Button 
-            onClick={handleEditCustomer}
-            variant="outlined" 
-            color="warning"
-            >
-              Editar
-          </Button>
-        </DialogActions>
+          <TextField
+            required
+            fullWidth
+            id="lastName"
+            label="Last Name"
+            name="lastName"
+            autoComplete="lname"
+            value={lastName}
+            onChange={e => setLastName(e.target.value)}
+          />
+          <TextField
+            required
+            fullWidth
+            type="tel"
+            inputProps={{ maxLength: 10 }}
+            id="phone"
+            label="Phone Number"
+            name="phone"
+            autoComplete="phone"
+            value={phoneNumber}
+            onChange={e => setPhoneNumber(e.target.value)}
+          />
+          <DialogActions>
+            <Button 
+              onClick={handleClose}
+              variant="outlined" 
+              color="success"
+              >
+                Cancelar
+            </Button>
+            <Button 
+              onClick={handleEditCustomer}
+              variant="outlined" 
+              color="warning"
+              type="submit"
+              >
+                Editar
+            </Button>
+          </DialogActions>
+        </form>
+        
       </Dialog>
     </div>
   );
