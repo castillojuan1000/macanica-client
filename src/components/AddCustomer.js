d3833401acd9b911debada9b06efb6e7a7b0bf7d
Juan Castillo <42900789+castillojuan1000@users.noreply.github.com>
2021-11-03 22:46:37 -0400

    update selectedTab

--- src/components/AddCustomer.js
+++ src/components/AddCustomer.js
@@ -54,7 +54,7 @@ function AddCustomer() {
             setDuplicatePhoneNumber(true)
           }else{
             window.localStorage.setItem('customer', JSON.stringify(response.data));
-            history.push('/profile')
+            window.location = '/profile'
           }
         })
         .catch(error => {
